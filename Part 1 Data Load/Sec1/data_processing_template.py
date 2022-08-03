b2f9d185c9c7d8da540861adea3563802769b57a
Linuxallan <84475280+Linuxallan@users.noreply.github.com>
2022-08-03 02:49:55 -0400

    seccion de regresion logistica completada y plantilla

--- Part 1 Data Load/Sec1/data_processing_template.py
+++ Part 1 Data Load/Sec1/data_processing_template.py
@@ -31,8 +31,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 ### Scalar datos, 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+X_train = sc_X.fit_transform(X_train) # fit_transform : crear escalado y aplicarlo (transform)
+X_test = sc_X.transform(X_test) # transform : solo se aplica (transform)
 """
 
 
